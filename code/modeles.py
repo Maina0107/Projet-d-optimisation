48cbb23c1ecda0db022253d268e3ea03194b196b
Maina Boivent <mboivent@emi.u-bordeaux.fr>
2025-01-23 10:49:29 +0100

    lancer et ecrire solution done

--- code/modeles.py
+++ code/modeles.py
@@ -2,6 +2,10 @@
 from data import PCentreData
 from solution import PCentreSolution
 
+import pyomo.environ as po
+from pyomo.core import quicksum
+import time 
+
 # ----------------------------------------------------------------------------
 #  Classe mère des modèles de p-Centre
 # ----------------------------------------------------------------------------
@@ -24,10 +28,45 @@ class ModelesPCentre:
         
 
     #_______________________ Méthode Résolution _______________________  
-    #def lancer(self, tempsLimite:int):
+    #lance la résolution du modèle avec le solveur souhaité tout en affichant son log
+    def lancer(self, tempsLimite:int):        
+        
+        solver_name ='appsi_highs'
 
+        if po.SolverFactory(solver_name).available():
+            print("Solver " + solver_name + " is available.")
+        else:
+            print("Solver " + solver_name + " is not available.")
 
+        # Création du solveur
+        solver = po.SolverFactory(solver_name)
+        solver.options['time_limit'] = tempsLimite
+        solver.options['mip_rel_gap'] = 1e-4
+        solver.options['mip_abs_gap'] = 0.99
 
+        start_time = time.time()
+        results = solver.solve(self.modele,tee=True)
+        end_time = time.time()
+
+        # Quel est le type de la solution retournée 
+        if(results.solver.termination_condition == po.TerminationCondition.optimal):   # Solution optmiale trouvée
+            self.statut = True
+        elif(results.solver.termination_condition == po.TerminationCondition.maxTimeLimit):  # Solution trouvée mais temps limite atteint 
+            self.statut = True
+        else:
+            self.statut = False 
+            print("Pas de solution calculée dans le temps limite (ou problème non borné)")
+
+        # Affichage du résultat
+        print("\n----------------------------------")
+        print(f'Temps de résolution (s) : {end_time - start_time:.4f} seconds')
+        print(f'Status du solveur = {results.solver.status}')
+        print(f'Status de la résolution = {results.solver.termination_condition}')
+        print(f'Valeur de la fonction objectif = {po.value(self.modele.obj)}')
+        print(f'Meilleure borne inférieure sur la valeur de la fonction objectif: { results.problem.lower_bound}')
+        print(f'Meilleure borne supérieure sur la valeur de la fonction objectif: { results.problem.upper_bound}')
+        print("----------------------------------")
+       
 
     #_______________________ Méthode Extraction de la Solution _______________________
     def extraire_solution(self, capacite: bool):
--- code/solution.py
+++ code/solution.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #Nommer le fichier dans lequel est écrite la solution : 
-#n2p1i1_v1c1.sol.txt : solution de l'intance n2p1i1 avec version 1 et avec capacité
+#n2p1i1_v1c1_1.sol.txt : solution de l'intance n2p1i1 avec version 1 et avec capacité
 #n2p1i1_v2c0.sol.txt : solution de l'intance n2p1i1 avec version 2 et sans capacité
 
 #Nommer fichier lp :
@@ -13,6 +13,7 @@ from numpy import ndarray
 import numpy as np
 from typing import List, Tuple
 from math import sqrt
+import pyomo.environ as po
 
 # ----------------------------------------------------------------------------
 #  Class solution
@@ -20,7 +21,7 @@ from math import sqrt
 class PCentreSolution:
     #_______________________ Attributs _______________________
 
-    def __init__(self, ouverture_installation : List[int] = None, affectation_client : List[int] = None, val_fonction : int = 0):
+    def __init__(self, ouverture_installation : List[int] = None, affectation_client : List[int] = None, val_fonction : float = 0):
 
         self.ouverture_installation = ouverture_installation if ouverture_installation is not None else []
 
@@ -32,36 +33,33 @@ class PCentreSolution:
     #_______________________ Méthodes _______________________
 
         
-    def ecriture_sol(self, instance_name: str, version: int, capacite: bool, irrealizable: bool = False):
-        """
-        Écrit la solution dans un fichier texte avec un nom adapté aux paramètres.
+    def ecriture_sol(self, instance_name: str, version: int, capacite: bool, versioncapa: int):
 
-        :param instance_name: Nom de l'instance (par ex. "n2p1i1").
-        :param version: Version du modèle (par ex. 1 ou 2).
-        :param capacite: Booléen indiquant si la capacité est utilisée (True/False).
-        """
         # Construire le nom du fichier en fonction des paramètres
         capacity_flag = "c1" if capacite else "c0"
-        filename = f"{instance_name}_v{version}{capacity_flag}.sol.txt"
+        if(capacite): 
+            filename = f"{instance_name}_v{version}{capacity_flag}_{versioncapa}.sol.txt"
+        else:
+            filename = f"{instance_name}_v{version}{capacity_flag}.sol.txt"
         
-        try:
-            with open(filename, "w") as file:
-                if irrealizable:
-                    # En cas de problème irréalisable, écrire -1 sur les trois lignes
-                    file.write("-1 -1 -1\n")
-                    file.write("-1 -1 -1\n")
-                    file.write("-1\n")
-                else:
-                    # Ligne 1 : Indicateur d'ouverture des installations (0 ou 1)
-                    ouverture_ligne = " ".join(str(int(i in self.ouverture_installation)) for i in range(len(self.affectation_client)))
-                    file.write(ouverture_ligne + "\n")
-                    
-                    # Ligne 2 : Affectations des clients aux installations
-                    affectations_ligne = " ".join(str(affectation) for affectation in self.affectation_client)
-                    file.write(affectations_ligne + "\n")
-                    
-                    # Ligne 3 : Valeur de la fonction objectif
-                    file.write(f"{self.val_fonction:.2f}\n")
+        solutionfilePath = filename
 
-        except Exception as e:
-            print(f"Erreur lors de l'écriture de la solution : {e}")
+        nb_noeuds = len(self.ouverture_installation)
+
+        with open(solutionfilePath, 'w') as file:
+
+            for i in range(nb_noeuds) :
+                file.write(str(self.ouverture_installation[i]))
+                file.write(" ")
+            
+            file.write("\n") #Je passe à la ligne suivante
+
+            for j in range(nb_noeuds):
+                    
+                file.write(str(self.affectation_client[j]))
+                file.write(" ")
+
+            file.write("\n") #Je passe à la ligne suivante
+
+            file.write(str(self.val_fonction))
+            
